7874005b12a21402224bcefe35aa73e2bd3195b4
FareedQ <quraishifareed@gmail.com>
2018-04-11 13:30:26 -0400

    SearchResults.js wasn't included in the last commit

--- SearchPage.js
+++ SearchPage.js
@@ -10,6 +10,8 @@ import {
   ActivityIndicator,
   Image,
 } from 'react-native';
+import SearchResults from './SearchResults';
+
 
 function urlForQueryAndPage(key, value, pageNumber) {
   const data = {
@@ -33,7 +35,7 @@ export default class SearchPage extends Component<{}> {
 	constructor(props) {
   	  super(props);
   	    this.state = {
-    	searchString: 'london',
+    	searchString: 'London',
   		isLoading: false,
   		message: '',
   	  };
@@ -44,7 +46,6 @@ export default class SearchPage extends Component<{}> {
 	};
 	
 	_executeQuery = (query) => {
-	  console.log(query);
 	  this.setState({ isLoading: true });
 	  fetch(query)
 	    .then(response => response.json())
@@ -64,7 +65,7 @@ export default class SearchPage extends Component<{}> {
 	_handleResponse = (response) => {
 	  this.setState({ isLoading: false , message: '' });
 	  if (response.application_response_code.substr(0, 1) === '1') {
-	    console.log('Properties found: ' + response.listings.length);
+	    this.props.navigator.push({title: 'Results', component: SearchResults, passProps: {listings: response.listings}});
 	  } else {
 	    this.setState({ message: 'Location not recognized; please try again.'});
   	  }
--- index.js
+++ index.js
@@ -2,3 +2,24 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('PropertyFinder', () => App);
+
+
+
+
+function urlForQueryAndPage(key, value, pageNumber) {
+  const data = {
+      country: 'uk',
+      pretty: '1',
+      encoding: 'json',
+      listing_type: 'buy',
+      action: 'search_listings',
+      page: pageNumber,
+  };
+  data[key] = value;
+
+  const querystring = Object.keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&');
+
+  return 'https://api.nestoria.co.uk/api?' + querystring;
+}
